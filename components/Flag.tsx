e1f9fc62fb88257dee9ca93132968bbcf03b87ca
Marco Quintella <2om.marco.quintella@gmail.com>
2022-12-31 14:05:43 -0300

    Flags on language list

--- components/Flag.tsx
+++ components/Flag.tsx
@@ -1,24 +1,39 @@
 import { Image } from '@rneui/themed';
 import { useEffect, useState } from 'react';
-import { TouchableOpacity } from 'react-native';
+import { TouchableOpacity, View } from 'react-native';
 import { useAppSelector } from '../store/hooks';
+import { Language } from '../types/language';
 
 const images: Record<string, any> = {
-  us: require('../assets/flags/united-states-of-america.png')
+  us: require('../assets/flags/united-states-of-america.png'),
+  brazil: require('../assets/flags/brazil.png'),
+  spain: require('../assets/flags/spain.png'),
 };
 
-export default function Flag () {
-  const language = useAppSelector(state => state.language);
+export default function Flag ({ button = true, language }: {
+  button: boolean;
+  language?: Language;
+}) {
+  const languageStore = useAppSelector(state => state.language);
+  const _language = () => language ? language : languageStore.currentLanguage;
 
   const [uri, setUri] = useState(require('../assets/flags/united-states-of-america.png'));
 
   useEffect(() => {
-    setUri(language?.currentLanguage?.flag ? language?.currentLanguage?.flag : 'us');
-  }, [language?.currentLanguage]);
+    setUri(_language()?.flag ?? 'us');
+  }, [language, languageStore.currentLanguage?.flag]);
 
-  return (
-    <TouchableOpacity>
-      <Image source={images[uri]} style={{ width: 32, height: 32 }} />
-    </TouchableOpacity>
-  );
+  const image = () => <Image source={images[uri]} style={{ width: 32, height: 32 }} />;
+
+  return button
+    ? (
+      <TouchableOpacity>
+        {image()}
+      </TouchableOpacity>
+    )
+    : (
+      <View>
+        {image()}
+      </View>
+    );
 }
--- screens/admin/AdminLanguagesScreen.tsx
+++ screens/admin/AdminLanguagesScreen.tsx
@@ -1,8 +1,9 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { Button, Header, Icon } from '@rneui/themed';
+import { Button, Header, Icon, Text } from '@rneui/themed';
 import { useEffect, useState } from 'react';
 import { View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import Flag from '../../components/Flag';
 import colors from '../../theme/colors';
 import { Language } from '../../types/language';
 import { fetchLanguages } from '../../utils/languages';
@@ -26,7 +27,11 @@ export default function AdminLanguagesScreen ({ navigation }: AdminLanguagesScre
   function languagesList () {
     const add = <Button key='add' title="New" containerStyle={{ width: '100%' }} />;
     return languages.map((language, index) => {
-      return <Button key={index} title={capitalize(language?.name)} containerStyle={{ marginTop: index !== 0 ? 16 : 0, width: '100%' }} />;
+      return (<Button
+        key={index} containerStyle={{ marginTop: index !== 0 ? 16 : 0, width: '100%' }}>
+        <Flag language={language} button={false} />
+        <Text style={{ color: colors.foreText, fontSize: 16, fontWeight: '700', marginLeft: 8 }} >{capitalize(language?.name)}</Text>
+      </Button>);
     });
   }
 
